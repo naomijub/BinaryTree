eef1e5f9bbebd1210d871304896ac67da92dc15b
naomijub <jnboeira@outlook.com>
2016-11-04 01:41:37 -0200

    Tetsing

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -3,10 +3,19 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "BinTree.h"
 
-
+using namespace std;
 int main()
 {
+	int i;
+	BinTree* bt = new BinTree();
+	cout<<bt->insert(5)<<endl;
+	cout << bt->insert(3) << endl;
+	cout << bt->insert(7) << endl;
+	cout << bt->insert(5) << endl;
+
+	cin >> i;
     return 0;
 }
 
